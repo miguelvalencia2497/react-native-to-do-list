6c5b821feee5a0a233245df8d33ba7e514973e84
miguelvalencia <miguelvalencia2497@gmail.com>
2017-09-28 22:43:22 +0800

    Main UI done

--- index.android.js
+++ index.android.js
@@ -9,23 +9,34 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput,
+  ScrollView,
+  TouchableOpacity
 } from 'react-native';
 
 export default class TodoListApp extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Todo App</Text>
+        </View>
+      
+
+        <ScrollView style={styles.scrollContainer}></ScrollView>
+
+        <View style={styles.footer}>
+          <TouchableOpacity style={styles.addButton}>
+            <Text style={styles.addButtonText}>+</Text>
+          </TouchableOpacity>
+
+          <TextInput style={styles.textInput}
+            placeholder='Add note'
+            placeholderTextColor='white'
+            underlineColorAndroid='transparent'>
+          </TextInput>
+        </View>
       </View>
     );
   }
@@ -34,20 +45,55 @@ export default class TodoListApp extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  header: {
+    backgroundColor: '#27ae60',
+    alignItems: 'center' ,
     justifyContent: 'center',
+    borderBottomWidth: 10,
+    borderBottomColor: '#ddd'
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 18,
+    padding: 26
+  },
+  scrollContainer: {
+    flex: 1,
+    marginBottom: 100,
+  },
+  footer: {
+    position: 'absolute',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    bottom: 0,
+    left: 0,
+    right: 0
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  addButton: {
+    backgroundColor: '#27ae60',
+    width: 90,
+    height: 90,
+    borderRadius: 50,
+    borderColor: '#ccc',
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 8,
+    marginBottom: -45,
+    zIndex: 10
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  addButtonText: {
+    color: '#fff',
+    fontSize: 24
   },
+  textInput: {
+    alignSelf: 'stretch',
+    color: '#fff',
+    padding: 20,
+    paddingTop: 40,
+    backgroundColor: '#252525',
+    borderTopWidth: 2,
+    borderTopColor: '#ededed'
+  }
 });
 
 AppRegistry.registerComponent('TodoListApp', () => TodoListApp);
